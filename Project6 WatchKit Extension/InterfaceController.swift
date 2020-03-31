ada83a7c9a6384aa3ade23887b1f4cda306cd069
look4yasharth <look4yasharth@gmail.com>
2020-03-31 07:05:24 +0530

    Voice Reminders

---
+++ Project6 WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,53 @@
+import Foundation
+import WatchKit
+
+class InterfaceController: WKInterfaceController {    
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+
+    @IBAction func dictationTapped() {
+        presentTextInputController(withSuggestions: nil, allowedInputMode: .plain) { result in
+            guard let result = result?.first as? String else { return }
+            print(result)
+        }
+    }
+
+    @IBAction func multiInputTapped() {
+        presentTextInputController(withSuggestions: ["Hacking with Swift", "Hacking with macOS", "Server-Side Swift"], allowedInputMode: .allowEmoji) { result in
+            guard let result = result?.first as? String else { return }
+            print(result)
+        }
+    }
+
+    @IBAction func recordingTapped() {
+        let saveURL = getDocumentsDirectory().appendingPathComponent("recording.wav")
+
+        if FileManager.default.fileExists(atPath: saveURL.path) {
+            let options = [WKMediaPlayerControllerOptionsAutoplayKey: "true"]
+
+            presentMediaPlayerController(with: saveURL, options: options) { didPlayToEnd, endTime, error in
+                // do nothing here
+            }
+        } else {
+            let options: [String: Any] =
+                [WKAudioRecorderControllerOptionsMaximumDurationKey : 60,
+                 WKAudioRecorderControllerOptionsActionTitleKey: "Done"]
+
+            presentAudioRecorderController(withOutputURL: saveURL, preset: .narrowBandSpeech, options: options) { success, error in
+                if success {
+                    print("Saved successfully!")
+                } else {
+                    print(error?.localizedDescription ?? "Unknown error")
+                }
+            }
+        }
+    }
+
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+}
